cc842696f1145069f85a7bbebdf9d578d55b7fe8
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-01-17 15:28:44 +0100

    Added all of the starting typescript files.
    Co-authored-by: Moa Hedendahl <moa.hedendahl@medieinstitutet.se>
    Co-authored-by: Nathalie Gustafsson <nathaliegustafsson@users.noreply.github.com>
    Co-authored-by: Gabriel Lugo Méndez <gabriel.lugomendez@medieinstitutet.se>
    Co-authored-by: Lisa Marie Andersson <lisamarie.andersson@medieinstitutet.se>
    Co-authored-by: Sebastianjohansson123 <Sebastianjohansson123@users.noreply.github.com>

---
+++ src/gameboard.ts
@@ -0,0 +1,10 @@
+class Gameboard {
+  // private entities: entity [];
+  // private backgrounds: Background[];
+  // private: number;
+  // private isGameOver: boolean;
+
+  constructor() {
+    //
+  }
+}
