67a9be8da133a90260245c6dd00f0e31fbf50fbd
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-01-19 09:51:11 +0100

    Added a score tracker + start for a function that repeats the background

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -1,14 +1,17 @@
 class GameBoard {
   // private entities: entity [];
   // private backgrounds: Background[];
-  // private: number;
   // private isGameOver: boolean;
   private shape: MainCharacter;
   private platforms: Platform[];
+  private score: number;
+  private scoreMultiplier: number = 1;
+  private timeSinceLastMultiplierIncrease: number = 0;
 
   constructor() {
     this.shape = new MainCharacter("./assets/images/bumpy.png");
     this.platforms = [], new Platform(50, 50, 100, 100, "./assets/images/platform.png");
+    this.score = 0;
     this.generatePlatforms();
   }
 
@@ -20,25 +23,36 @@ class GameBoard {
   }
 
   public draw() {
+    this.drawBackground();
     this.shape.draw();
+    this.getScore();
     this.platforms;
     this.platforms.forEach(platform => platform.draw());
   }
 
-  // checks if the shape(the moving rectangle) gets in contact with the upper part of
-  // the platforms and triggers an automatic jump if it does
+  // repeats the background image
+  // can possibly be adjusted later on to include different backgrounds depending on the height
+  private drawBackground() {
+    let repeatCount = height / bg.height + 1;
+    for (let i = 0; i < repeatCount; i++) {
+      image(bg, 0, i * bg.height);
+    }
+  }
+
+  // checks if the MainCharacters gets in contact with the platform 
+  // when it is falling and triggers an automatic jump if it is
   private detectCollision() {
     for (let platform of this.platforms) {
       if (this.shape.getPosition().y + this.shape.getSize().y > platform.getPosition().y 
           && this.shape.getPosition().y + this.shape.getSize().y < platform.getPosition().y + platform.getSize().y
-          && (this.shape.getPosition().x + this.shape.getSize().x > platform.getPosition().x 
-              && this.shape.getPosition().x < platform.getPosition().x + platform.getSize().x)
-              && (this.shape.getVelocity().y > -1)) {
+          && (this.shape.getPosition().x + this.shape.getSize().x  - 20 > platform.getPosition().x // added "- 20" to better adjust hitbox
+              && this.shape.getPosition().x + 20 < platform.getPosition().x + platform.getSize().x) // // added "+ 20" to better adjust hitbox
+              && (this.shape.getVelocity().y > 1)) { // Makes it so that the MainCharacter only jumps on the platforms if is falling at a certain velocity
         this.shape.jump();
       }
     }
   }
-  // randomly creates the X position for the platform but makes sure that there is always a 200 px gap
+  // randomly creates the X position for the platform but makes sure that there is always a 120 px gap
   // between the height of each platform
   private generatePlatforms() {
     let y = height;
@@ -47,32 +61,49 @@ class GameBoard {
       let x = random(0, width - 220);
       let platform = new Platform(x, y, 220, 20, "./assets/images/platform.png");
       this.platforms.push(platform);
-      y -= 200;
+      y -= 120;
     }
   }
+  
   private updatePlatforms() {
     // a for each loop that removes a platform if y is lower than 0 and pushes a new one
+    // and keeps track of the score by increasing it for each platform that reaches the bottom
+    // and gets removed plus adds a multiplier for the score that makes the platform award
+    // a higher score the further that the player gets in the game
     for (let i = 0; i < this.platforms.length; i++) {
-      let platform = this.platforms[i];
-      if (platform.getPosition().y > height) {
-        this.platforms.splice(i, 1);
-        let x = random(0, width - 220);
-        let newPlatform = new Platform(x, 0, 220, 20, "./assets/images/platform.png");
-        this.platforms.push(newPlatform);
-        // this.score++
-      }
+        let platform = this.platforms[i];
+        if (platform.getPosition().y > height) {
+            this.platforms.splice(i, 1);
+            let x = random(0, width - 220);
+            let newPlatform = new Platform(x, 0, 220, 20, "./assets/images/platform.png");
+            this.platforms.push(newPlatform);
+            this.score += 1 * this.scoreMultiplier;
+            this.timeSinceLastMultiplierIncrease += 1;
+            if (this.timeSinceLastMultiplierIncrease >= 10) {
+                this.scoreMultiplier += 1;
+                this.timeSinceLastMultiplierIncrease = 0;
+            }
+        }
     }
-  }
+}
 
   private movePlatforms() {
     if (
       // This value controls how high you can jump before the platforms move
-      this.shape.getPosition().y < 100
+      this.shape.getPosition().y < 220
     ) {
       // a for each loop that moves the platforms down
       for (let platform of this.platforms) {
-        platform.getPosition().y += 4;
+        platform.getPosition().y += 5;
+        // this.shape.getPosition().y += 0;
       }
     }
+  }
+
+  // Function to track the score of the current game and display it in the top-left corner
+  private getScore() {
+    fill(0);
+    textSize(21);
+    text("Score: " + this.score, 10, 30);
   }
 }
