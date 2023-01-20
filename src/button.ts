5c33591fc94b3d2af5e63dcb63219c51dea41bc6
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-01-20 12:46:22 +0100

    added code for button in draw and new buttons

--- src/button.ts
+++ src/button.ts
@@ -1,32 +1,51 @@
-// class Button {
-//   private content: string | p5.Image;
-//   private position: p5.Vector;
-//   private size: p5.Vector;
-//   // private radius: number;
+class Button {
+   private content: string | p5.Image;
+   private position: p5.Vector;
+   private size: p5.Vector;
+   private radius: number;
 
-//   constructor(content: string | p5.Image, position: p5.Vector, size: p5.Vector) {
-//     this.content = content;
-//     this.position = position;
-//     this.size = size;
-//     // this.radius = size.x * 0.1
-//   } 
+   constructor(content: string | p5.Image, position: p5.Vector, size: p5.Vector) {
+     this.content = content;
+     this.position = position;
+     this.size = size;
+     this.radius = size.x * 0.1
+   } 
 
 
 //   public update() {
 //     // Klickade användaren på knappen?
 //   }
 
-//   public draw() {
-//     push();
-//     fill("#873333");
-//     // 1. rita rectangle
-//     // 2. rita texten
-//     // if (typeof this.content === "string") {
-//     //   text()
-//     // } else {
-//     //   image()
-//     // }
-//     // 3. rita ikon bilder
-//     pop();
-//   }
-// }
+public draw() {
+   push();
+   // 1. rita rektangel
+   noStroke();
+   fill("#DFADD0");
+   rectMode(CENTER);
+   rect(this.position.x, this.position.y, this.size.x, this.size.y, this.radius);
+   // 2. rita texten
+   fill("#3A1458");
+   textSize(22);
+   textFont("TitanOne");
+   if (typeof this.content === "string") {
+      textAlign(CENTER, CENTER);
+      text(this.content, this.position.x, this.position.y);
+   } else {
+      imageMode(CENTER);
+      image(this.content, this.position.x, this.position.y, this.size.x, this.size.y);
+   }
+   pop();
+  }
+}
+
+
+// Created buttons for EndMenu
+let btnPlayAgain = new Button("PLAY AGAIN", new p5.Vector(100, 150), new p5.Vector(300, 100));
+let btnMenu = new Button("MENU", new p5.Vector(100, 200), new p5.Vector(200, 50));
+
+
+
+
+
+
+
