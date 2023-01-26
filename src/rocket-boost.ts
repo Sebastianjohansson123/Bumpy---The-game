5cb9bcd3c0a4dd2bd227bb5b50fe05d04ef1fc6d
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-01-26 20:32:38 +0100

    Reverted changes made in last commit -WRONG branch

--- src/rocket-boost.ts
+++ src/rocket-boost.ts
@@ -1,20 +1,3 @@
 /// <reference path="entity.ts"  />
 class RocketBoost extends Entity {
-  constructor(position: p5.Vector) {
-    super(
-      position,
-      createVector(0, 0),
-      createVector(0, 0),
-      createVector(50, 90),
-      images.bumpySad
-    );
-  }
-
-  public getPosition(): p5.Vector {
-    return this.position;
-  }
-
-  public getSize(): p5.Vector {
-    return this.size;
-  }
 }
